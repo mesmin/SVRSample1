cc6430d7aa5a430f0b88720551edb47bde892443
mesmin@gmail.com <mesmin@gmail.com>
2020-11-18 22:06:21 +0000

    File generated by Skaffolder - user: mesmin@gmail.com

---
+++ src/app/domain/svrsample1_db/vendor.ts
@@ -0,0 +1,9 @@
+import { VendorBase } from './base/vendor.base';
+
+/**
+ * YOU CAN OVERRIDE HERE VendorBase.ts
+ */
+export interface Vendor extends VendorBase {
+
+    // Insert here your custom attributes and function
+}
---
+++ src/app/services/vendor.service.ts
@@ -0,0 +1,17 @@
+// BASE SERVICE
+import { VendorBaseService } from './base/vendor.base.service';
+
+
+// start documentation
+/**
+ * Custom APIs
+ *
+ */
+// end documentation
+
+/**
+ * YOU CAN OVERRIDE HERE VendorBaseService
+ */
+export class VendorService extends VendorBaseService {
+
+}
